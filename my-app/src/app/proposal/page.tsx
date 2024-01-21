6f609d0d80d13816ab621ceb64a923f4988aca1c
Mokshith1708 <146767844+Mokshith1708@users.noreply.github.com>
2024-01-21 16:10:30 +0530

    Todays Project

---
+++ my-app/src/app/proposal/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function Proposal(){
+    return(
+        <div className="w-Screen h-screen bg-pink-500">
+            <div>
+                <img/>
+                <p></p>
+                <Button></Button>
+                <Button></Button>
+            </div>
+        </div>
+    );
+}
